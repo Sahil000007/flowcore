2cf1efa313272eb00e5ab8c3eef7c656c63340be
Sahil <sahilsingh20211@gmail.com>
2026-06-12 23:29:03 +0530

    updated

---
+++ setup_db.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE IF NOT EXISTS flowcore_db;
+USE flowcore_db;
+
+-- The tables will be auto-created by Hibernate when the application starts
+-- Based on the entities defined in the application
+
+SHOW DATABASES;
